17b8965ede8e4078910bf29c400108eb950f2fa4
Wojciech Musialkiewicz <gambiting@gmail.com>
2012-09-30 00:49:20 +0100

    project commit

---
+++ console/console/console.cpp
@@ -0,0 +1,15 @@
+// console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	printf("hello world");
+
+	printf("h2");
+	return 0;
+}
+
